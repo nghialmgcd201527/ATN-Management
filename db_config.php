1fca47a98f7c3a70916644c66f336812b6b18d05
lmnghia911 <lmnghia911@gmail.com>
2022-07-06 09:37:26 +0700

    second commit

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+$host_name = "ec2-34-239-241-121.compute-1.amazonaws.com";
+$db_name = "d54b7pfdqbl79m";
+$db_user = "aijumnntwofzqh";
+$db_pwd = "a708c110bbe58a5beb81c891a8ed2fed32372a73316c5682f6ff5f84137b9864";
+$db_conn_string = "host=$host_name dbname=$db_name user=$db_user password=$db_pwd";
+?>
